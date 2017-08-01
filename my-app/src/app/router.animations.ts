adec1bcd50fc6c3bbf953e83e4e0bc5b4bf5cf11
Smalli <Smalli@DESKTOP-9M2SRI6>
2017-08-01 13:15:07 +0800

    添加动画元素

---
+++ my-app/src/app/router.animations.ts
@@ -0,0 +1,13 @@
+import {trigger, animate, style, group, animateChild, query, stagger, transition} from '@angular/animations';
+
+export const routerTransition = trigger('routerTransition', [
+	transition('* <=> *', [
+		/* 1 */
+		query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
+		/* 2 */
+		group([
+			query(':enter', [style({ transform: 'translateX(100%)' }), animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))], {optional: true}), 
+			query(':leave', [style({ transform: 'translateX(0%)'}), animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))], {optional: true})
+		])
+	])
+])
